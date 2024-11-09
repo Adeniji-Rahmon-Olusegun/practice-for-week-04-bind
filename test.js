05dc9e2e36f6188552531182a1995642e07d3af7
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-11-10 00:01:01 +0100

    practice completed

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-// Your code here
+const Employee = require("./employee");
+
+const emp = new Employee("John Wick", "Dog Lover");
+
+const unboundEmp = emp.sayName;
+
+const boundEmp = unboundEmp.bind(emp);
+
+global.setTimeout(boundEmp, 2000);
+
+/***********************************************************/
+const unboundEmpOcc = emp.sayOccupation;
+
+const boundEmpOcc = unboundEmpOcc.bind(emp);
+
+global.setTimeout(boundEmpOcc, 3000);
+
